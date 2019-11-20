3a02694f882c7330a4de9bdbf45765dab7e02e92
Med Amine Ben Chouikha <med.amine.ben.chouikha@gmail.com>
2019-11-20 23:49:14 +0200

    begin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,51 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/seller/register/register.component';
+import { LoginComponent } from './components/seller/login/login.component';
+import { Page404Component } from './components/page404/page404.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+    ]
+  },
+  {
+    path :'seller',
+    children:[
+      {
+        path:'category',
+        children:[
+          
+        ]
+      },
+      {
+        path:'product',
+        children:[
+
+        ]
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/seller/register/register.component';
+import { LoginComponent } from './components/seller/login/login.component';
+import { Page404Component } from './components/page404/page404.component';
+import { CategoriesListComponent } from './components/seller/category/categories-list/categories-list.component';
+import { CategoriesAddComponent } from './components/seller/category/categories-add/categories-add.component';
+import { CategoryEditComponent } from './components/seller/category/category-edit/category-edit.component';
+import { ProductsListComponent } from './components/seller/product/products-list/products-list.component';
+import { ProductAddComponent } from './components/seller/product/product-add/product-add.component';
+import { ProductEditComponent } from './components/seller/product/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    Page404Component,
+    CategoriesListComponent,
+    CategoriesAddComponent,
+    CategoryEditComponent,
+    ProductsListComponent,
+    ProductAddComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
